d0f222b9d7855ef57e1ddb9f9f2231c632f17793
Master-Bibash <dokfreak142@gmail.com>
2024-01-21 19:05:46 +0545

    day two dot net

--- Day-one/swap.cs
+++ Day-one/swap.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 
-namespace Swap{
-     class Swap
+
+
+class Swap
+{
+
+     static void Main(String[] args)
      {
-          static void Main(String[] args)
-          {
-               Console.WriteLine("enter a value");
-               int a = Convert.ToInt16(Console.ReadLine());
-               Console.WriteLine("enter b value");
-               int b = Convert.ToInt16(Console.ReadLine());
-               a = a + b;
-               b = a - b;
-               a=a-b;
+          // Prompt the user to enter the value for 'a'
+          Console.WriteLine("Enter a value");
+          int a = Convert.ToInt16(Console.ReadLine());
 
-               // Console.WriteLine("Swapping a and be we get of a is {0} and b i s {1}", a,b);
-               Console.WriteLine($"swapping a and b value we get {a} and {b}");
-          }
+          // Prompt the user to enter the value for 'b'
+          Console.WriteLine("Enter b value");
+          int b = Convert.ToInt16(Console.ReadLine());
+
+          // Swap the values of 'a' and 'b' without using a third variable
+          a = a + b;    // Sum of 'a' and 'b' is stored in 'a'
+          b = a - b;    // 'b' is updated to be the difference between the new 'a' and original 'b'
+          a = a - b;    // 'a' is updated to be the difference between the original sum and new 'b'
+
+          // Display the swapped values
+          Console.WriteLine($"Swapping a and b values, we get {a} and {b}");
      }
-
 }
