2c3bd6354421f83e05fdfcc74790d87fc3e00afb
Master-Bibash <dokfreak142@gmail.com>
2024-02-04 21:50:57 +0545

    day six oop

---
+++ Day-Six/Example/QuestionThree.cs
@@ -0,0 +1,129 @@
+
+     using System;
+     using System.Security.Cryptography.X509Certificates;
+     namespace DaySix.Example
+     {
+
+          class VSNI
+          {
+          static void Main(string[] args)
+          {
+               VSi vsObject = new VSi();
+               bool continueExecution = true;
+
+             do
+             {
+                    Console.WriteLine("Enter\n 1 to admit. \n 2 to add faculty. \n 3 to restrict.");
+
+                    //so here int.parse is a method that tries to parse a string as an integer.The out int userOPtion  part means if parsing is successfull the parsed interger will be stored in userOPtion if fails tryparse will return false and userOPtion will have default value if 0
+                    if (int.TryParse(Console.ReadLine(),out int userOPtion))
+                    {
+                         switch(userOPtion)
+                         {
+                              case 1:
+                              vsObject.Admission();
+                                   break;
+                              case 2:
+                                   vsObject.AddFaculty();
+                                   break;
+                              case 3:
+                                   vsObject.Restrict();
+                                   break;
+                              default:
+                                   Console.WriteLine("Please select option 1, 2, or 3.");
+                                   break;
+                         }
+                    }
+                    else{
+                         Console.WriteLine("Invalid input. Please enter a valid number.");
+
+                    }
+                    Console.WriteLine("Do you want to perform another action? (y/n)");
+                    string continueOption = Console.ReadLine().ToLower();
+                    if (continueOption != "y")
+                    {
+                         continueExecution = false;
+                    }
+
+
+
+
+               } while (continueExecution);
+          }
+     }
+}
+          
+
+
+     class VSi
+     {
+          private string[] faculty = new string[3];
+          private int studentNum;
+
+          public int StudentNum
+          {
+               get { return studentNum; }
+          //Inn this specific case, when you write private set { studentNum = value; }, it means that the private property studentNum can only be modified within the class, and the new value assigned to it is the value provided by the set operation, which is represented by the value keyword.
+          //so new value will be stored in studentNum which is value
+          private set { studentNum = value; }
+          }
+          public string[] Faculty{
+               get
+               {
+               return faculty;
+               }
+               private set{
+               faculty = value;
+               }
+          }
+
+          public void Admission()
+          {
+               Console.WriteLine("Enter student number to admit: ");
+               if (int.TryParse(Console.ReadLine(), out int part1))
+               {
+                    StudentNum += part1;
+                    Console.WriteLine("Number of students admitted: " + StudentNum);
+               }
+               else
+               {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+               }
+          }
+
+          public void Restrict()
+          {
+               if (StudentNum > 0)
+               {
+                    Console.WriteLine("Enter number to restrict: ");
+                    if (int.TryParse(Console.ReadLine(), out int num))
+                    {
+                        if(studentNum<=num){
+                         StudentNum -= num;
+                         Console.WriteLine("Total number of students is now: " + StudentNum);
+                    }else{
+                         Console.WriteLine("value cannot be grater than student");
+                    }
+                    }
+                    else
+                    {
+                         Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+               }
+          }
+
+          public void AddFaculty()
+          {
+               for (int i = 0; i < Faculty.Length; i++)
+               {
+                    Console.WriteLine($"Enter faculty member {i+1}");
+               Faculty[i] = Console.ReadLine();
+                    
+               }
+               foreach (var facultymember in Faculty)
+               {
+               Console.WriteLine("faculty are " + facultymember);
+                    
+               }
+          }
+     }
--- Day-Six/Example/QuestionTwo.cs
+++ Day-Six/Example/QuestionTwo.cs
@@ -3,11 +3,14 @@
 //make methods admission,resticate and add faculty they are public
 
 using System;
-namespace DaySix.Example{
+namespace DaySix.Example
+{
 
-     class VSN{
-          static void Main(string[] args){
-               VS vsobject=new VS();
+     class VSN
+     {
+          static void Main(string[] args)
+          {
+               VS vsobject = new VS();
                vsobject.admission();
                vsobject.AddFaculty("MAth");
                vsobject.Restrict(1);
@@ -17,25 +20,31 @@ namespace DaySix.Example{
      {
           private string[] studnetfacuty;
           private int studentNum;
-          public VS(){
+          public VS()
+          {
                studnetfacuty = new string[3];
                studentNum = 0;
 
           }
-        
+
 
           //method for admission
-          public void admission(){
+          public void admission()
+          {
                Console.WriteLine("student is admitted");
                studentNum++;
           }
           //method for resticating
-          public void Restrict(int studentNum){
-               if(studentNum>0){
-                    for(int i=0;i<studnetfacuty.Length;i++){
-                         if(studnetfacuty[i]!=null && i+1==studentNum){
+          public void Restrict(int studentNum)
+          {
+               if (studentNum > 0)
+               {
+                    for (int i = 0; i < studnetfacuty.Length; i++)
+                    {
+                         if (studnetfacuty[i] != null && i + 1 == studentNum)
+                         {
                               Console.WriteLine($"Student with number {studentNum} restricted. Faculty removed: {studnetfacuty[i]}");
-                              studnetfacuty[i]=null;
+                              studnetfacuty[i] = null;
                               studentNum--;
                               return;
 
@@ -44,22 +53,25 @@ namespace DaySix.Example{
                     }
                     Console.WriteLine($"No student found with number {studentNum}.");
 
-               }else{
+               }
+               else
+               {
                     Console.WriteLine("No students to restrict.");
 
                }
           }
 
           //method for adding faculty
-          public void AddFaculty(string facultyName){
+          public void AddFaculty(string facultyName)
+          {
                for (int i = 0; i < studnetfacuty.Length; i++)
                {
-                if (studnetfacuty[i]==null)
-                {
+                    if (studnetfacuty[i] == null)
+                    {
                          studnetfacuty[i] = facultyName;
                          Console.WriteLine("Faculty added: " + facultyName);
                          return;
-                    }    
+                    }
                }
                Console.WriteLine("Maximum faculty limit reached.");
 
