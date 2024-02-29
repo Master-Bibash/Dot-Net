a3895f21c52fad48cc566b81cf08ca330a6d9cc0
Master-Bibash <dokfreak142@gmail.com>
2024-02-29 22:34:04 +0545

    delegate

---
+++ Day-Ten/Example/Three.cs
@@ -0,0 +1,24 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Example
+{
+
+
+     class Three
+     {
+          public delegate void calculate(int a, int b);
+
+
+          static void Main(String[] args)
+          {
+               
+               
+              
+
+          }
+     }
+}
+
+//operation add,sub,dev, using delegate and lamda ,
+// for example calculate(10,20) should call all the funcion add sub mul
+//remeber calculate is delegate
