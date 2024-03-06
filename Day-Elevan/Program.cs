d7f3935ccfd995a34918daf2d8f7bd793be0fb9c
Master-Bibash <dokfreak142@gmail.com>
2024-03-06 22:36:48 +0545

    all

---
+++ Day-Elevan/Program.cs
@@ -0,0 +1,6 @@
+﻿//Example of LInq query using Query syntex
+//LINQ stands for "Language Integrated Query" and pronounced as "LINK". LINQ was introduced with .NET Framework 3.5 including Visual Studio 2008, C# 3.0 and VB.NET 2008 (VB 9.0). It enables you to query the data from the various data sources like SQL databases, XML documents, ADO.NET Datasets, Web services and any other objects such as Collections, Generics etc. by using a SQL Query like syntax with .NET framework languages like C# and VB.NET.
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
